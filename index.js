56d2718565e0444952d24550bed7fc4691cc590d
Abhinandan <abhinandan@pop-os.localdomain>
2019-11-03 00:40:07 +0530

    added files

---
+++ index.js
@@ -0,0 +1,44 @@
+let x=1;
+let y=1;
+let z=1;
+
+
+let sigma=10;
+let beta=8/3;
+let rho=28;
+
+let pos=[]
+
+let prevPoint;
+let green;
+let red;
+
+function setup(){
+  createCanvas(windowWidth,windowHeight,WEBGL);
+}
+
+function draw(){
+  background(0);
+  let dt=0.01;
+  let dx=(sigma*(y-x))*dt;
+  let dy=(x* (rho-z)-y) *dt;
+  let dz=(x * y-beta *z)*dt;
+  x+=dx;
+  y+=dy;
+  z+=dz;
+  pos.push(createVector(x,y,z));
+  orbitControl();
+  scale(10);
+  noFill();
+  // print(pos.length);
+
+  for (i=0;i<pos.length;i++){
+    red =map(pos[i].x,-50,0,0,255);
+    green= map(pos[i].y,-50,0,0,255);
+    stroke(red,green,255);
+    if(i>0){
+      line(pos[i-1].x,pos[i-1].y,pos[i-1].z,pos[i].x,pos[i].y,pos[i].z);
+  }
+
+}
+}
